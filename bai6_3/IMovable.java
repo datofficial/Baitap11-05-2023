278240a716a83d66f494c04fe274a0077c0fe852
datofficial <ddatofficial89@gmail.com>
2023-05-11 11:47:11 +0700

    message

---
+++ bai6_3/IMovable.java
@@ -0,0 +1,8 @@
+package day4.baitapoop.bai6_3;
+
+public interface IMovable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
---
+++ bai6_3/MovablePoint.java
@@ -0,0 +1,40 @@
+package day4.baitapoop.bai6_3;
+
+public class MovablePoint implements IMovable{
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d) speed=(%d,%d)", x,y,xSpeed,ySpeed);
+    }
+
+    @Override
+    public void moveUp() {
+        y = y - ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        y = y + ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        x = x - xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        x = x + xSpeed;
+    }
+}
